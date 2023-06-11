79e4eb5da6af6466a893ee3d8add5c1178e222f4
ThonnyRG <136212279+ThonnyRG@users.noreply.github.com>
2023-06-11 20:50:45 +0000

    I finish with the all classes, now I gonna test the code, and maybe I will make some changes

---
+++ AddressBookApplication.java
@@ -0,0 +1,13 @@
+/**
+ * La clase AddressBookApplication contiene la clase principal de la aplicación.
+ * Se encarga de crear una instancia de AddressBook, una instancia de Menu y
+ * llamar al método displayMenu() de Menu para mostrar el menú y capturar las
+ * entradas del usuario.
+ */
+public class AddressBookApplication {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        Menu menu = new Menu(addressBook);
+        menu.displayMenu();
+    }
+}
