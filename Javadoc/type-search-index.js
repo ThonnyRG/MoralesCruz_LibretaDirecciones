621b4b0c3728e4123ca0a40006559118278213ca
ThonnyRG <136212279+ThonnyRG@users.noreply.github.com>
2023-06-13 16:46:46 +0000

    It's only the JavaDoc

---
+++ Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Address"},{"p":"<Unnamed>","l":"AddressBook"},{"p":"<Unnamed>","l":"AddressBookApplication"},{"p":"<Unnamed>","l":"AddressEntry"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Menu"}];updateSearchResults();
